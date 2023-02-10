9552a4582ab76949b16064bf2f58a8fde7fa96c0
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-02-10 11:29:03 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let gridSize = 10;
+let cellNumber = gridSize * gridSize
+
+const gridBox = document.querySelector('.grid-box');
+
+for(let i = 0 ; i < cellNumber ; i++) {
+    let num = i + 1;
+    let divString = `<div class="cell" style="width: calc(100% / ${gridSize});" >${num}</div>`
+
+    gridBox.innerHTML += divString;
+}
+
+const numberOnClick = document.querySelectorAll('.grid-box > .cell')
+
+for(let i = 0 ; i < numberOnClick.length ; i++){
+    const cell = numberOnClick[i]
+    cell.addEventListener('click', hello);
+};
+
+function hello(){
+    console.log('ciao');
+};
